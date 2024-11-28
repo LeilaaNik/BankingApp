412a29834a8dd7b41e51662a58b639e4e3b23eeb
Leila Nikkheslat <ln223kd@student.lnu.se>
2024-11-28 22:57:12 +0100

    REFACTOR: Add JavaDoc comments and override toString method in Account class

---
+++ app/src/main/java/banking/app/Account.java
@@ -0,0 +1,45 @@
+package banking.app;
+
+/**
+ * Represents a bank account with an account ID and balance.
+ */
+public class Account {
+    private String accountId;
+    private double balance;
+
+    /**
+     * Constructs a new Account with the specified account ID and an initial balance of 0.
+     *
+     * @param accountId the unique identifier for the account
+     */
+    public Account(String accountId) {
+        this.accountId = accountId;
+        this.balance = 0;
+    }
+
+    /**
+     * Returns the account ID.
+     *
+     * @return the account ID
+     */
+    public String getAccountId() {
+        return accountId;
+    }
+
+    /**
+     * Returns the current balance of the account.
+     *
+     * @return the current balance
+     */
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountId='" + accountId + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
